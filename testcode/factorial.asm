80bc36d1a94dac5a826b4949a2fce09cb0d33955
Rishi Thakkar <rishirt.us@gmail.com>
2017-01-22 23:13:39 -0600

    MP0 Completed. Need to move files around and clean up folder.

--- testcode/factorial.asm
+++ testcode/factorial.asm
@@ -1,3 +1,10 @@
+
+
+;; Factorial Program
+;; Author: Rishi Thakkar (RRTHAKK2)
+;; Date: 1/22/2017
+;; Assignment: MP0
+
 ORIGIN 4x0000
 
 SEGMENT  CodeSegment:
@@ -11,36 +18,48 @@ SEGMENT  CodeSegment:
 	;; R6 - R7 are unused
 
 	;; Load input and constant data
-    LDR R1, R0, INPUT		;; R1 <= Input
-	LDR R5, R0, NEG_ONE		;; R4 <= -1
-	ADD R2, R1, R5			;; R2 <= Input - 1
+    LDR R1, R0, INPUT					;; R1 <= Input
+	LDR R5, R0, NEG_ONE					;; R5 <= -1
+	ADD R2, R1, R5						;; R2 <= Input - 1
 
 FACTORIAL_LOOP:
-	;; Check counter variable to see if program needs to continue
-	AND R2, R2, R5			;; Sets cc
-	BRnz HALT				;; if(R2 <= 0), then factorial has been calculated
+	;; Check to see if iterative work completed
+	AND R2, R2, R5						;; Sets cc
+	BRnz HALT							;; if(R2 <= 0), then factorial has been calculated
 
 	;; Setup up values for MULT_LOOP
-	ADD R3, R2, R5			;; R3 <= R2 - 1 
-	ADD R4, R1, R0			;; R4 <= R1 ; sets R4 to current total
-	MULT_LOOP:
-		;; Check counter variable to see if program needs to continue
-		AND R3, R3, R5			;; Sets cc
-		BRnz MULT_DONE			;; if(R3 <= 0), then factorial has been calculated
+	ADD R3, R2, R5						;; R3 <= R2 - 1 
+	ADD R4, R1, R0						;; R4 <= R1 ; sets R4 to current total
+MULT_LOOP:
+	;; Check to see if iterative work completed
+	AND R3, R3, R5						;; Sets cc
+	BRnz MULT_DONE						;; if(R3 <= 0), then factorial has been calculated
 
-		ADD R1, R1, R4 			;; add intermediate value to total
-
-		ADD R3, R3, R5			;; decrement counter
-		BRnzp MULT_LOOP
+	ADD R1, R1, R4 						;; add intermediate value to total
+	
+	;; decrement counter and jump to start of MULT_LOOP
+	ADD R3, R3, R5			
+	BRnzp MULT_LOOP
 
 MULT_DONE:
-	ADD R2, R2, R5 			;; decrement counter
+	;; decrement counter and jump to start of FACTORIAL_LOOP
+	ADD R2, R2, R5
 	BRnzp FACTORIAL_LOOP
 
 HALT:
     BRnzp HALT
 
-INPUT:    DATA2 4x0006		;; Change this variable to modify input into factorial program
+INPUT:    DATA2 4x0005		;; Change this variable to modify input into factorial program
 
 ;; General Constants
 NEG_ONE:  DATA2 4xFFFF		;; used for decrementing a register within the program
+
+
+
+
+
+
+
+
+
+
