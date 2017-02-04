e9c01479d1e5dc04eb9c697c7f09c527b02559f6
Atrifex <rishirt.us@gmail.com>
2017-02-03 19:56:33 -0600

    Wrote some more tests.

---
+++ testcode/MP1CP2/mp1-test-and.asm
@@ -0,0 +1,69 @@
+;;
+;; This program is a limited test of AND
+;; An all-encompassing test for this instruction will be performed separately in dedicated programs.
+;; In the comments you can find the expected end results for each register.
+;; Other end conditions will also be speficied to make sure that success has actually occured.
+;;
+;; Author: Rishi Thakkar
+;; Date: 2/2/2017
+;; Assignment: MP1
+;;
+
+
+
+ORIGIN 0
+SEGMENT
+CODE:
+
+	;; ************* Expected End Results *************
+	;; R0 - R7: Used for many tasks
+	;;
+	;; To check for correctness, you will need to compare the
+	;; register values after every instruction and that the contents
+	;; of memory in ModelSim match the contents of memory in the
+	;; LC3bSimulator.
+	;;
+
+	;; Prepare register values
+	LEA R0, DATA
+	LDR R1, R0, TVAL_1
+	LDR R2, R0, TVAL_2
+	LDR R3, R0, TVAL_3
+	LDR R4, R0, TVAL_4
+	LDR R5, R0, TVAL_5
+	LDR R6, R0, TVAL_6
+	LDR R7, R0, TVAL_7
+	LDR R0, R0, TVAL_8
+
+	;; Testing AND with registers
+	AND R1, R1, R2
+	AND R1, R1, R3
+	AND R1, R1, R4
+	AND R1, R1, R5
+	AND R1, R1, R6
+	AND R1, R1, R7
+	AND R1, R1, R0			;; Make sure R1 = 0x01FF
+
+	;; Testing AND imm.
+	LEA R0, DATA
+	LDR R0, R0, TVAL_1
+	AND R0, R0, #15
+	AND R0, R0, #7
+	AND R0, R0, #3
+	AND R0, R0, #1
+	AND R0, R0, #0			;; Make sure R0 = 0xFFF0
+
+HALT_PROG:
+	BRnzp HALT_PROG
+
+SEGMENT
+DATA:
+ZERO: 	DATA2 4x0000
+TVAL_1: DATA2 4xFFFF
+TVAL_2: DATA2 4x7FFF
+TVAL_3: DATA2 4x3FFF
+TVAL_4: DATA2 4x1FFF
+TVAL_5: DATA2 4x0FFF
+TVAL_6: DATA2 4x07FF
+TVAL_7: DATA2 4x03FF
+TVAL_8: DATA2 4x01FF
---
+++ testcode/MP1CP2/mp1-test-jsr.asm
@@ -0,0 +1,70 @@
+;;
+;; This program is a limited test of JSR and JSRR.
+;; An all-encompassing test for each instruction will be performed separately in dedicated programs.
+;; In the comments you can find the expected end results for each register.
+;; Other end conditions will also be speficied to make sure that success has actually occured.
+;;
+;; Author: Rishi Thakkar
+;; Date: 2/2/2017
+;; Assignment: MP1
+;;
+
+
+ORIGIN 0
+SEGMENT
+CODE:
+
+	;; ************* Expected End Results *************
+	;; R0: 0
+	;; R1: -1
+	;; R2: 3
+	;; R3: 4
+	;; R4: 2
+	;; R5: Not Used
+	;; R6: Used to store address to load/store/call
+	;; R7: Used to store return address
+	;;
+	;; Make sure that the contents of memory in ModelSim match
+	;; the contents of memory in the LC3bSimulator.
+	;;
+
+	LEA R6, LDB_TVAL
+	LDB R1, R6, 0			;; load byte: LDB_TVAL + 0
+	LDB R2, R6, 1			;; load byte: LDB_TVAL + 1
+
+	;; Testing JSR
+	JSR ADD_FUNC
+
+	LEA R6, STB_TVAL
+	STB R3, R6, 0 			;; store byte: STB_TVAL + 0
+
+	;; Testing JSRR
+	LEA R6, SUB_FUNC
+	JSRR R6
+
+	LEA R6, STB_TVAL
+	STB R4, R6, 1 			;; store byte: STB_TVAL + 0
+
+HALT_PROG:
+	BRnzp HALT_PROG
+
+;; input: R1, R2
+;; output: R3
+ADD_FUNC:
+	ADD R3, R2, R1
+	RET
+
+;; input: R1, R2
+;; output: R4 = R2 - R1
+SUB_FUNC:
+	;; 2's complement
+	NOT R1, R1
+	ADD R1, R1, 1
+
+	ADD R4,R2, R1
+	RET
+
+SEGMENT
+DATA:
+LDB_TVAL: 		DATA2 4x0301 ;; Args for testing STB, LDB, JSR, and JSRR
+STB_TVAL:  		DATA2 ?
