d87e402cf1c58868965643e714fcec65f7a6ee80
Rishi Thakkar <rishirt.us@gmail.com>
2017-02-16 20:37:29 -0600

    Compressed the MP0 and MP1 files
    Makes project size smaller.

--- mp1/simulation/modelsim/rtl_work/cccomp/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity cccomp is
-    port(
-        cur_cc          : in     vl_logic_vector(2 downto 0);
-        br_cc           : in     vl_logic_vector(2 downto 0);
-        br_enable       : out    vl_logic
-    );
-end cccomp;
